00341ad7133aebc2b0cd084f3d795b69709af459
Endriw Braga <endriwbragalisboa@gmail.com>
2024-11-30 21:08:32 -0300

    Correção 2 exercicio proposto p melhoria de entrada de dados, agora é possivel colocar nomes compostos.

--- course02/src/application/Storage.java
+++ course02/src/application/Storage.java
@@ -10,17 +10,20 @@ public class Storage {
 
 		Scanner sc = new Scanner(System.in);
 
-		// define o tamanho do array
+		System.out.print("Quantos itens deseja registrar? ");
 		int lengthArray = sc.nextInt();
+		sc.nextLine(); // Consumir a linha em branco '' BUFFER ''
 
-		String[] nameIten = new String[lengthArray];
+		String[] itemNames = new String[lengthArray];
 		double[] valueItens = new double[lengthArray];
 
 		for (int i = 0; i < lengthArray; i++) {
 			System.out.print("Digite o nome do item: ");
-			nameIten[i] = sc.next();
+			itemNames[i] = sc.nextLine();
+
 			System.out.print("Digite o valor do item: ");
 			valueItens[i] = sc.nextDouble();
+			sc.nextLine(); // Consumir a linha em branco '' BUFFER ''
 		}
 
 		double sumOfValues = 0;
@@ -33,13 +36,15 @@ public class Storage {
 
 		System.out.println("\nItens digitados:");
 		for (int i = 0; i < lengthArray; i++) {
-			System.out.println("Nome: " + nameIten[i] + ", Valor: " + valueItens[i]);
+			System.out.println("Nome: " + itemNames[i] + ", Valor: " + valueItens[i]);
 		}
 
 		System.out.printf("\nA media dos valores é: %.2f%n ", valuesMedia);
 
 		sc.close();
 
+		// Há necessidade de quando o nome digitado for um numero pelo usuario, eu
+		// converter p string p imprimir?
+		// hoje não, mas no futuro? ....
 	}
-
 }
